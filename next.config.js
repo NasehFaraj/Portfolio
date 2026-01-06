616b3f318c3e77ab19bdb845b65db6dcbc90f7a4
Naseh Faraj <nasehfara@gmail.com>
2026-01-06 23:26:41 +0300

    Set base path env for static assets

--- next.config.js
+++ next.config.js
@@ -7,6 +7,9 @@ const nextConfig = {
   images: {
     unoptimized: true
   },
+  env: {
+    NEXT_PUBLIC_BASE_PATH: isProduction ? "/Portfolio" : ""
+  },
   basePath: isProduction ? "/Portfolio" : "",
   assetPrefix: isProduction ? "/Portfolio/" : undefined
 };
