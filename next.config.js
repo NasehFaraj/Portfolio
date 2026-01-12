abadab0c9e80e295c53e8446eee26b677485c744
Naseh Faraj <nasehfara@gmail.com>
2026-01-12 14:11:16 +0300

    Simplify Next config and stabilize Pages build

--- next.config.js
+++ next.config.js
@@ -1,22 +1,10 @@
 /** @type {import('next').NextConfig} */
-const rawBasePath = process.env.NEXT_PUBLIC_BASE_PATH || "";
-const withLeadingSlash = rawBasePath
-  ? rawBasePath.startsWith("/") ? rawBasePath : `/${rawBasePath}`
-  : "";
-const basePath =
-  withLeadingSlash.length > 1 && withLeadingSlash.endsWith("/")
-    ? withLeadingSlash.slice(0, -1)
-    : withLeadingSlash;
-
-module.exports = {
+const nextConfig = {
   output: "export",
   trailingSlash: true,
   images: {
     unoptimized: true
-  },
-  env: {
-    NEXT_PUBLIC_BASE_PATH: basePath
-  },
-  basePath,
-  assetPrefix: basePath ? `${basePath}/` : undefined
+  }
 };
+
+module.exports = nextConfig;
